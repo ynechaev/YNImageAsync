16c274b000d3b8548f40a518d0086895afd7f0ee
Yury Nechaev <nechaev.main@gmail.com>
2016-10-16 21:14:01 +0200

    Run synx and changed log level to errors for demo.

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
         
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        YNImageAsync.setLoggingLevel(level: .info)
+        YNImageAsync.setLoggingLevel(level: .errors)
         return true
     }
 }
