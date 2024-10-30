ea386f41d00c1fddf8ba5a04bbf3e2f8b1993c31
ynechaev <nechaev.main@gmail.com>
2024-10-30 21:11:23 +0000

    Skip performance tests.

--- Tests/YNImageAsyncTests/PerformanceTests.swift
+++ Tests/YNImageAsyncTests/PerformanceTests.swift
@@ -16,11 +16,13 @@ final class PerformanceTests: XCTestCase {
     }
     
     func test_diskStore_performance() async throws {
+        XCTSkip
         // 10k writes ~ 1.2s on macbook pro M1
         try await runPerformanceTest(with: [.disk], timeout: 5)
     }
     
     func test_memoryStore_performance() async throws {
+        XCTSkip
         // 10k writes ~ 0.2s on macbook pro M1
         try await runPerformanceTest(with: [.memory], timeout: 2)
     }
